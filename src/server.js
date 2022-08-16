00c26e7619dd95a037c0281e04bd26b19533d3aa
Bom <11377ny@gmail.com>
2022-08-15 21:10:12 -0400

    Adjust server.js for deploy

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 import http from "http";
-import SocketIO from "socket.io";
+import { Server } from "socket.io";
 import express from "express";
 
 const app = express();
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
@@ -11,7 +17,7 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer);
 
 const sockets = [];
 
